10fefbbcba79ba365fef2b192864bb0f446ca349
haekyu <gorb6593@gmail.com>
2024-06-22 19:40:16 +0900

    커밋

---
+++ src/main/java/com/example/harry2/Harry2Application.java
@@ -0,0 +1,13 @@
+package com.example.harry2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Harry2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Harry2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/harry2/Harry2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harry2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Harry2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
